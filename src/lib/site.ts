b75d351da64e6a831357f15fca06f9a03c761ba5
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-10-01 09:27:55 +0000

    modification du domaine

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
   name: "Miss Athlète Côte d'Ivoire - Célébrons le Sport Féminin Ivoirien",
-  url: "https://missathlete.ci",
+  url: "https://miss-athlete.com",
   description: "Miss Athlète met en lumière les athlètes féminines ivoiriennes par une compétition unique alliant sport, force et élégance. Découvrez l'événement !",
   keywords:["sport féminin", "Côte d'Ivoire", "Miss Athlète", "Cynthia Kragbé", "taekwondo", "compétition sportive", "élégance"],
   links: {
-    twitter: "https://x.com/dillionverma",
-    github: "https://github.com/dillionverma",
+    twitter: "",
+    github: "",
   },
 };
 
